002eecb418596e74f9dc23c07abf0ec991a0eb19
Saifullah-Afridi <saifullahafridi08@gmail.com>
2026-01-03 21:46:41 +0500

    removed the global.css warning message by creating a file called global.d.ts in root and added a line declare module astric.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="winter">
-      <body>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
