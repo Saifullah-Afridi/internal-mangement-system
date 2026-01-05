a4432ccb651dfd85522396b63a95d7045db792cf
Saifullah-Afridi <saifullahafridi08@gmail.com>
2026-01-05 14:23:42 +0500

    add a fake authentication on server side and dashboard redring based on this authentication

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,13 @@
-export default function Dashboard() {
+import { isAuthenticated } from "@/lib/auth";
+
+export default async function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      {!(await isAuthenticated()) ? (
+        <h1>Access is denied.Please login again</h1>
+      ) : (
+        <h1>Admin dashboard</h1>
+      )}
     </div>
   );
 }
